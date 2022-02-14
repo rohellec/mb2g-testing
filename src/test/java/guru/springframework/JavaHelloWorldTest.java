c9b4bd8d2a254a4a7b3b6838200293dfbf787faa
Dima Rogov <ds.rogv@gmail.com>
2022-02-15 00:44:11 +0300

    Add JUnit4 test

---
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -0,0 +1,16 @@
+package guru.springframework;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class JavaHelloWorldTest {
+
+    @Test
+    public void getHello() {
+        JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
+        
+        assertEquals("Hello World", javaHelloWorld.getHello());
+    }
+
+}
