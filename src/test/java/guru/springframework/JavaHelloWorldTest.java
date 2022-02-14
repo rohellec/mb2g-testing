9d0f47cfb2d455b30f90ea0e71554cce86560db7
Dima Rogov <ds.rogv@gmail.com>
2022-02-15 01:39:45 +0300

    Add JUnit5 test

---
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -0,0 +1,16 @@
+package guru.springframework;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class JavaHelloWorldTest {
+
+    @Test
+    void getHello() {
+        JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
+        
+        assertEquals("Hello World", javaHelloWorld.getHello());
+    }
+
+}
